efe9c6a547e1e38bafeca762a6a33d4182077169
Felix Schlepper <f3sch.git@outlook.com>
2022-06-26 17:14:27 +0200

    Added jiffies
    Signed-off-by: Felix Schlepper <f3sch.git@outlook.com>

--- task_08/hello.c
+++ task_08/hello.c
@@ -8,6 +8,7 @@
 #include <linux/debugfs.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/jiffies.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Felix Schlepper");
@@ -15,9 +16,10 @@ MODULE_DESCRIPTION("Challenge 01 - Hello, world!");
 MODULE_VERSION("1.0");
 
 #define YOUR_ID_LEN 10
-const static char name_buf[YOUR_ID_LEN] =
+static char name_buf[YOUR_ID_LEN] =
 	"Eudyptula"; // I don't have an id, just using this
 static struct dentry *dbgfs; // global parent
+static char jbuf[sizeof(jiffies)];
 
 static ssize_t eduyptula_write(struct file *file, const char __user *buf,
 			       size_t len, loff_t *ppos)
@@ -52,23 +54,55 @@ static ssize_t eduyptula_read(struct file *file, char __user *buf, size_t len,
 	return YOUR_ID_LEN;
 }
 
-static const struct file_operations fops = {
+static const struct file_operations efops = {
 	.owner = THIS_MODULE,
 	.write = eduyptula_write,
 	.read = eduyptula_read,
 };
 
+static ssize_t jiffies_write(struct file *file, const char __user *buf,
+			     size_t len, loff_t *ppos)
+{
+	return 0;
+}
+
+static ssize_t jiffies_read(struct file *file, char __user *buf, size_t len,
+			    loff_t *ppos)
+{
+	sprintf(jbuf, "%lu", jiffies);
+	if (copy_to_user(buf, jbuf, sizeof(jiffies))) {
+		pr_debug("Jiffies: Read: Could not copy to user space!\n");
+		return -EFAULT;
+	}
+
+	return sizeof(jiffies);
+}
+
+static const struct file_operations jfops = {
+	.owner = THIS_MODULE,
+	.write = jiffies_write,
+	.read = jiffies_read,
+};
+
 static int __init start(void)
 {
-	struct dentry *id;
+	struct dentry *id, *jif;
 	dbgfs = debugfs_create_dir("eudyptula", NULL);
 	if (!dbgfs) {
 		pr_err("Could not create debugfs dir!\n");
 		return -ENODEV;
 	}
 
-	id = debugfs_create_file("id", S_IRWXU, dbgfs, (void *)name_buf, &fops);
+	id = debugfs_create_file("id", S_IRWXU, dbgfs, (void *)name_buf,
+				 &efops);
 	if (!id) {
+		pr_err("Could not create id file!\n");
+		goto err_id;
+	}
+
+	jif = debugfs_create_file("jiffies", S_IRUSR | S_IROTH, dbgfs,
+				  (void *)jbuf, &jfops);
+	if (!jif) {
 		pr_err("Could not create id file!\n");
 		goto err_id;
 	}
